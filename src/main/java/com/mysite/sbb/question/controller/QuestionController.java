7d8177608c9dad2506fcfe1bdc71d737dbdb1c0b
FullSoYu <hey6410@naver.com>
2022-09-29 04:56:33 +0900

    지ì질문 상세 페이지

---
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -0,0 +1,44 @@
+package com.mysite.sbb.question.controller;
+
+import com.mysite.sbb.question.domain.Question;
+import com.mysite.sbb.question.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class QuestionController {
+
+
+    private final QuestionService questionService;
+
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/question/list";
+    }
+
+
+    @RequestMapping("/question/list")
+    public String list(Model model) {
+
+        List<Question> questionList = questionService.getList();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+
+    @RequestMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable Integer id) {
+        System.out.println("id : " + id);
+        Question question = questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+
+    }
+
+
+}
